632a2f002938c5ace91bea3355ac318c89e03f15
vyudu <vincent.duyuan@gmail.com>
2025-11-09 16:36:20 -0500

    add another comment

--- plot_utils.jl
+++ plot_utils.jl
@@ -3,6 +3,8 @@
 # Plot utilities
 p3 = plot(sol_spline, label=["α" "Cp" "αCp" "CppC" "RNA"], lw=2, xlabel = "Time", ylabel = "Concentration", title = "System Species over time with optimal V", legend = :topright, size = (1200, 900))
 
+# Add another comment
+
 # Plot costates λ₁ to λ₅ over time
 p4 = plot(
           costate_trajectory, label=["λ₁" "λ₂" "λ₃" "λ₄" "λ₅"], lw=2,
